cd74c0baf53dd5f8174dfad13abfb29a65df63c8
NiniPomelo <n0966785407@gmail.com>
2024-12-05 15:13:12 +0800

    week13

---
+++ week13/week13-3.cpp
@@ -0,0 +1,16 @@
+// week13-3.cpp 解 Advent of code 2024 Day 1 題目
+// LeetCode 右上角圖示, My Playground
+// New Playground, +新的 Playground
+// 前面 LeetCode 幫妳寫好 #include <iostream> 和 #include <vector>
+// using namespace std; 都幫妳寫好了, 你不用寫, 方便你在遊樂場玩程式
+int main() {
+    vector<int> A, B; // 2個陣列 (伸縮自如)
+    int a, b; // 兩個數
+    while( cin >> a >> b) { // Step01: Input
+        A.push_back(a); // Step02: 放到陣列
+        B.push_back(b);
+    }
+    for(int i=0; i<A.size(); i++) { // Step02: Output
+        cout << A[i] << " ";
+    }
+}
